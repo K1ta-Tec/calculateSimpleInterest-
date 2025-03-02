8f1b606dbface27f19d70a536059d468d3f2f3be
Kita Abang <eyareabang@gmail.com>
2025-03-02 21:53:49 +0100

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,31 @@
+The formula for Simple Interest (SI) is:
+
+𝑆
+𝐼
+=
+𝑃
+×
+𝑅
+×
+𝑇
+100
+SI= 
+100
+P×R×T
+​
+ 
+Where:
+
+P = Principal amount
+R = Rate of interest per annum
+T = Time in years
+
+# Given values for simple interest calculation
+P = 1000  # Principal amount in dollars
+R = 5      # Rate of interest per annum in percentage
+T = 3      # Time in years
+
+# Simple Interest formula
+SI = (P * R * T) / 100
+SI
 
